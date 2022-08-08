92ee6fe898f7a3b73e4d09baff79e3a0d6b5361a
Onur Özbay <ayon1524@gmail.com>
2022-08-08 03:23:58 +0300

    add readme.md

--- ticketco-payment/src/main/java/com/ticketco/ticketcopayment/TicketcoPaymentApplication.java
+++ ticketco-payment/src/main/java/com/ticketco/ticketcopayment/TicketcoPaymentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MooviePaymentApplication {
+public class TicketcoPaymentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MooviePaymentApplication.class, args);
+        SpringApplication.run(TicketcoPaymentApplication.class, args);
     }
 
 }
